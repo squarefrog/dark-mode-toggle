bae29530f1158c14b9cebfaa9746a6b325d585d0
Paul Williamson <squarefrog@gmail.com>
2019-03-21 19:34:04 +0000

    Initial commit

---
+++ DarkThemeToggle/SettingsViewController.swift
@@ -0,0 +1,62 @@
+//
+//  Copyright © 2019 squarefrog. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UITableViewController, Themeable {
+    @IBOutlet weak var darkModeCell: UITableViewCell! {
+        didSet {
+            let darkModeSwitch = UISwitch()
+            darkModeSwitch.addTarget(self, action: #selector(toggleDarkMode(_:)), for: .valueChanged)
+            darkModeSwitch.onTintColor = Theme.dark.accentColor
+            darkModeCell.accessoryView = darkModeSwitch
+        }
+    }
+
+    var currentTheme: Theme = .light {
+        didSet {
+            apply(theme: currentTheme)
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = super.tableView(tableView, cellForRowAt: indexPath)
+        cell.apply(theme: currentTheme)
+        return cell
+    }
+
+    @objc private func toggleDarkMode(_ sender: UISwitch) {
+        let center = sender.superview?.convert(sender.center, to: nil) ?? .zero
+
+        // Circular
+        CircularReveal.animate(from: center) { _ in
+            self.setNeedsStatusBarAppearanceUpdate()
+        }
+
+        // Linear
+//        LinearReveal.animate { _ in
+//            self.setNeedsStatusBarAppearanceUpdate()
+//        }
+
+        currentTheme = sender.isOn ? .dark : .light
+    }
+
+    func apply(theme: Theme) {
+        let navigationBar = navigationController?.navigationBar
+        navigationBar?.barTintColor = theme.navigationBarColor
+        navigationBar?.titleTextAttributes = [.foregroundColor: theme.navigationTextColor]
+
+        tableView.backgroundColor = theme.backgroundColor
+        tableView.separatorColor = theme.cellSeparatorColor
+
+        for cell in tableView.visibleCells {
+            cell.apply(theme: currentTheme)
+        }
+
+    }
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return currentTheme.statusBarStyle
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Dark Mode Toggle 🌌
+
+After seeing the animation when toggling Dark Theme in Slack, I thought I'd try my hand at replicating the animation. After that I wondered how I could spice up the animation by using a circular reveal.
+
+## Linear Reveal
+
+The new view is revealed top to bottom.
+
+## Circular Reveal
+
+The new view is animated out by an ever increasing circular mask, from a center point.
+
